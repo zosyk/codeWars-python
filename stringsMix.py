c09b3189a4250db1e6886a71f3ae2a00f9ff47fa
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-03-06 23:00:06 +0200

    kata "Strings Mix" - refactoring

--- stringsMix.py
+++ stringsMix.py
@@ -1,6 +1,3 @@
-from functools import cmp_to_key
-
-
 def mix(s1, s2):
     firstMap = createMapFromString(s1)
     secondMap = createMapFromString(s2)
@@ -18,7 +15,7 @@ def mix(s1, s2):
         if v > 1:
             result.append("2:" + k * v)
 
-    return "/".join(sorted(result, key=cmp_to_key(cmp_items), reverse=True))
+    return "/".join(sorted(result, key=lambda s: (-len(s), s)))
 
 
 def createMapFromString(s):
@@ -27,26 +24,3 @@ def createMapFromString(s):
         if char.islower():
             map[char] = map.get(char, 0) + 1
     return map
-
-
-def cmp_items(a, b):
-    if len(a) > len(b):
-        return 1
-    elif len(a) < len(b):
-        return -1
-    else:
-        if a[0] == "1" and not b[0] == "1":
-            return 1
-        elif b[0] == "1" and not a[0] == "1":
-            return -1
-        elif a[0] == "2" and b[0] == "=":
-            return 1
-        elif a[0] == "=" and b[0] == "2":
-            return -1
-
-        if a < b:
-            return 1
-        elif a > b:
-            return -1
-        else:
-            return 0
