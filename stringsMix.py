98f98e63727efa24468aa2cd9218fe48cb1b137d
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-03-06 09:09:38 +0200

    kata "Strings Mix" - initial implementation

--- stringsMix.py
+++ stringsMix.py
@@ -1,3 +1,52 @@
+from functools import cmp_to_key
+
+
 def mix(s1, s2):
-    # your code
-    pass
+    firstMap = createMapFromString(s1)
+    secondMap = createMapFromString(s2)
+    result = []
+
+    for k, firstValue in firstMap.items():
+        secondValue = secondMap.pop(k, 0)
+        if firstValue > secondValue and firstValue > 1:
+            result.append("1:" + k * firstValue)
+        elif secondValue > firstValue:
+            result.append("2:" + k * secondValue)
+        elif firstValue == secondValue and firstValue > 1:
+            result.append("=:" + k * firstValue)
+    for k, v in secondMap.items():
+        if v > 1:
+            result.append("2:" + k * v)
+
+    return "/".join(sorted(result, key=cmp_to_key(cmp_items), reverse=True))
+
+
+def createMapFromString(s):
+    map = {}
+    for char in s:
+        if char.islower():
+            map[char] = map.get(char, 0) + 1
+    return map
+
+
+def cmp_items(a, b):
+    if len(a) > len(b):
+        return 1
+    elif len(a) < len(b):
+        return -1
+    else:
+        if a[0] == "1" and not b[0] == "1":
+            return 1
+        elif b[0] == "1" and not a[0] == "1":
+            return -1
+        elif a[0] == "2" and b[0] == "=":
+            return 1
+        elif a[0] == "=" and b[0] == "2":
+            return -1
+
+        if a < b:
+            return 1
+        elif a > b:
+            return -1
+        else:
+            return 0
--- tests/test_mix.py
+++ tests/test_mix.py
@@ -5,22 +5,19 @@ from stringsMix import mix
 class TestMix(TestCase):
 
     def test1(self):
-        self.assertEqual(mix("Are they here", "yes, they are here"), "2:eeeee/2:yy/=:hh/=:rr")
+        self.assertEqual("2:eeeee/2:yy/=:hh/=:rr", mix("Are they here", "yes, they are here"))
 
     def test2(self):
-        self.assertEqual(mix("looping is fun but dangerous", "less dangerous than coding"),
-                         "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg")
+        self.assertEqual("1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg", mix("looping is fun but dangerous", "less dangerous than coding"))
 
     def test3(self):
-        self.assertEqual(mix(" In many languages", " there's a pair of functions"),
-                         "1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt")
+        self.assertEqual("1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt", mix(" In many languages", " there's a pair of functions"))
 
     def test4(self):
-        self.assertEqual(mix("Lords of the Fallen", "gamekult"), "1:ee/1:ll/1:oo")
+        self.assertEqual("1:ee/1:ll/1:oo", mix("Lords of the Fallen", "gamekult"))
 
     def test5(self):
-        self.assertEqual(mix("codewars", "codewars"), "")
+        self.assertEqual("", mix("codewars", "codewars"))
 
     def test6(self):
-        self.assertEqual(mix("A generation must confront the looming ", "codewarrs"),
-                         "1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr")
+        self.assertEqual("1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr", mix("A generation must confront the looming ", "codewarrs"))
