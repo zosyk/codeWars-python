6351e462cf3aca945d946553abf329d973378f77
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-03-10 15:05:03 +0200

    kata "Conway's Game of Life - Unlimited Edition" - refactoring

--- conwaysGame.py
+++ conwaysGame.py
@@ -1,27 +1,19 @@
 def get_generation(cells, generations):
+    if not cells:
+        return [[]]
+
+    xm, ym, xM, yM = 0, 0, len(cells[0]), len(cells)
+    cells = {(x, y) for y, r in enumerate(cells) for x, v in enumerate(r) if v}
     for g in range(generations):
-        cells = expand(cells)
-        cells = [[next_cell(cells, i, j) for j in range(len(cells[i]))] for i in range(len(cells))]
-    trim(cells)
-    return cells
+        cells = {(x, y) for x in range(xm - 1, xM + 2) for y in range(ym - 1, yM + 2)
+                 if 2 < len(cells & get_neighbors(x, y)) < 4 + ((x, y) in cells)}
+    xm = min(x for x, y in cells)
+    ym = min(y for x, y in cells)
+    xM = max(x for x, y in cells)
+    yM = max(y for x, y in cells)
+
+    return [[int((x, y) in cells) for x in range(xm, xM + 1)] for y in range(ym, yM + 1)]
 
 
-def next_cell(cells, i, j):
-    live_cells = sum([get(cells, i + x, j + y) for x in range(-1, 2) for y in range(-1, 2)])
-    return int(2 < live_cells < 4 + cells[i][j])
-
-
-def expand(cells):
-    row = [0] * (len(cells[0]) + 2)
-    return [row] + [[0] + r + [0] for r in cells] + [row]
-
-
-def trim(cells):
-    while not any(cells[0]): del cells[0]
-    while not any(cells[-1]): del cells[-1]
-    while not any([row[0] for row in cells]): [row.pop(0) for row in cells]
-    while not any([row[-1] for row in cells]): [row.pop() for row in cells]
-
-
-def get(cells, i, j):
-    return cells[i][j] if len(cells) > i > -1 < j < len(cells[i]) else 0
+def get_neighbors(x, y):
+    return {(x + n, y + m) for n in range(-1, 2) for m in range(-1, 2)}
